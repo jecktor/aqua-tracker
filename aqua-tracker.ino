7c7795756b5a6e04ed7d21ac8ae312081bc7bd49
jecktor <jecktor@proton.me>
2023-11-28 10:47:38 -0600

    feat: added tds sensor code

--- aqua-tracker.ino
+++ aqua-tracker.ino
@@ -1,12 +1,13 @@
 #include <NewPing.h>
 #include <TimedAction.h>
-#include <GravityTDS.h>
 
 #define trigPin 27
 #define echoPin 28
 #define MAX_DISTANCE 500
 
 #define tdsPin 26
+#define VREF 5.0   // analog reference voltage(Volt) of the ADC
+#define SCOUNT 30  // sum of sample point
 
 void updateSonar();
 void updateTds();
@@ -16,23 +17,25 @@ NewPing sonar(trigPin, echoPin, MAX_DISTANCE);
 TimedAction sonarAction = TimedAction(100, updateSonar);
 TimedAction tdsAction = TimedAction(1000, updateTds);
 
-GravityTDS gravityTds;
-
 byte dataPin = 18;
 byte latchPin = 20;
 byte clockPin = 21;
 
 const byte comPin[] = { 17, 16, 15, 14 };
-const byte rgbPins[] = { 13, 12, 11 };
+const byte rgbPins[] = { 11, 12, 13 };
 
 const byte num[] = { 0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8,
                      0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e };
 
 uint16_t distance = 0;
 
-float temperature = 25, tdsValue = 0;
+int analogBuffer[SCOUNT];  // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0, copyIndex = 0;
+float averageVoltage = 0, tdsValue = 0, temperature = 25;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
@@ -44,10 +47,7 @@ void setup() {
     pinMode(comPin[i], OUTPUT);
   }
 
-  gravityTds.setPin(tdsPin);
-  gravityTds.setAref(5.0);       // reference voltage on ADC, default 5.0V
-  gravityTds.setAdcRange(1024);  // 1024 for 10bit ADC;4096 for 12bit ADC
-  gravityTds.begin();            // initialization
+  pinMode(tdsPin, INPUT);
 }
 
 void loop() {
@@ -79,8 +79,6 @@ void loop() {
 
     n /= 10;
   }
-
-  // update rgb led
 }
 
 void updateSonar() {
@@ -88,9 +86,42 @@ void updateSonar() {
 }
 
 void updateTds() {
-  gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
-  gravityTds.update();
-  tdsValue = gravityTds.getTdsValue();
+  static unsigned long analogSampleTimepoint = millis();
+  if (millis() - analogSampleTimepoint > 40U)  //every 40 milliseconds,read the analog value from the ADC
+  {
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(tdsPin);  //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if (analogBufferIndex == SCOUNT)
+      analogBufferIndex = 0;
+  }
+  static unsigned long printTimepoint = millis();
+  if (millis() - printTimepoint > 800U) {
+    printTimepoint = millis();
+    for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+    averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 1024.0;                                                                                                   // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);                                                                                                                //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+    float compensationVolatge = averageVoltage / compensationCoefficient;                                                                                                             //temperature compensation
+    tdsValue = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge) * 0.5;  //convert voltage value to tds value
+    Serial.print("TDS Value:");
+    Serial.print(tdsValue, 0);
+    Serial.println("ppm");
+  }
+  // Update rgb led
+  if (tdsValue <= 170) {
+    analogWrite(rgbPins[0], 255);
+    analogWrite(rgbPins[1], 0);
+    analogWrite(rgbPins[2], 0);
+  } else if (tdsValue <= 400) {
+    analogWrite(rgbPins[0], 0);
+    analogWrite(rgbPins[1], 255);
+    analogWrite(rgbPins[2], 0);
+  } else if (tdsValue > 400) {
+    analogWrite(rgbPins[0], 255);
+    analogWrite(rgbPins[1], 255);
+    analogWrite(rgbPins[2], 0);
+  }
 }
 
 void electDigitalDisplay(byte com) {
@@ -106,3 +137,23 @@ void writeData(int value) {
   digitalWrite(latchPin, HIGH);
 }
 
+int getMedianNum(int bArray[], int iFilterLen) {
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0)
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  else
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  return bTemp;
+}
